019ef8b5483b35f9008b7d4f5326c27d04294a12
Diogo Ferreira <defer@cyngn.com>
2016-06-16 18:17:09 +0100

    porridgek3: permissions: Declare usb host support
    This variant supports usb host, this patch enables it.
    
    Change-Id: I05707f566d7589ddc129c899c5943cbce4e19fc3

--- device.mk
+++ device.mk
@@ -4,23 +4,15 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit from porridge
-$(call inherit-product, device/wileyfox/porridge/device.mk)
-$(call inherit-product, device/wileyfox/porridgek3/device.mk)
-
-PRODUCT_DEVICE := porridgek3
-PRODUCT_NAME := full_porridgek3
-PRODUCT_BRAND := wileyfox
-PRODUCT_MODEL := PORRIDGEK3
-PRODUCT_MANUFACTURER := wileyfox
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml
